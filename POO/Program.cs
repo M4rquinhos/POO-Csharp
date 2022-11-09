da2baa675b33e5555cf1500e6ddefa062185b1fa
Marco Carrillo <marco.37099p@gmail.com>
2022-11-09 15:56:43 -0600

    Agregar archivos de proyecto.

---
+++ POO/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using POO;
+
+Console.WriteLine("Hello, World!");
+
+
+var carro = new Carro();
+carro.Marca = "Toyota";
+carro.EncenderRadio();
+
+
+var camion = new Camion();
+camion.Marca = "Torton";
+//camio.EncenderRadio(); //No se puede acceder a al metodo "EncederRadio", porque ese metodo sólo le pertence a la clase Carro
---
+++ POO/Vehiculo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    internal class Vehiculo
+    {
+        public string? Marca { get; set; }
+        public int AñoCreacion { get; set; }
+    }
+
+    internal class Carro : Vehiculo
+    {
+        public void EncenderRadio() 
+        {
+            Console.WriteLine("Encendiendo la Radio, pa");
+        }
+
+    }
+
+    internal class Camion : Vehiculo
+    {
+
+    }
+
+}
